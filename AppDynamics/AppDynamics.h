c3898c33b3fbbafea6b0c98f89d8619bede902f4
Bojan Savic <savicbojanks@gmail.com>
2019-05-01 16:03:54 +0200

    Initial Commit

---
+++ AppDynamics/AppDynamics.h
@@ -0,0 +1,19 @@
+//
+//  AppDynamics.h
+//  AppDynamics
+//
+//  Created by Bojan Savic on 5/1/19.
+//  Copyright © 2019 Bojan Savic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppDynamics.
+FOUNDATION_EXPORT double AppDynamicsVersionNumber;
+
+//! Project version string for AppDynamics.
+FOUNDATION_EXPORT const unsigned char AppDynamicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppDynamics/PublicHeader.h>
+
+
